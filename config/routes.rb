85e982d3a3957d307e59fe826994e72291a6a083
matt-duell <mkduell@gmail.com>
2017-02-17 17:57:15 -0500

    reordered the routing priority

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  resources :users
   get '/users/profile', to: 'users#profile'
+  resources :users
   post '/authenticate', to: 'authentication#authenticate'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
