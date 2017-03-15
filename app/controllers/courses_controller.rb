40935658b9cefdc18b7c4734b6937638e870ca69
Aaron-Tang <aar.tang@mail.utoronto.ca>
2017-03-15 19:18:00 -0400

    Need to finish tests for course API

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -1,45 +1,27 @@
 class CoursesController < ApplicationController
-	before_action :set_course, only: [:show, :update, :destroy]
+	before_action :set_course, only: [:show, :update, :destroy, :applicants]
 
-  # GET /users
+  # GET /courses
   def index
     @courses = Course.all
 
     render json: @courses
   end
 
-  # GET /users/1
+  # GET /courses/1
   def show
     render json: @course
   end
 
-  # POST /users
-  def create
-    @course = Course.new(user_params)
+  # GET /courses/id/applicants
+  def applicants
+  	@applications = @course.ta_applications
 
-    if @course.save
-      render json: @course, status: :created, location: @course
-    else
-      render json: @course.errors, status: :unprocessable_entity
-    end
-  end
+  	# Ruby Map function
 
-  # PATCH/PUT /users/1
-  def update
-    if @course.update(user_params)
-      render json: @course
-    else
-      render json: @course.errors, status: :unprocessable_entity
-    end
-  end
-
-  # DELETE /users/1
-  def destroy
-    @course.destroy
-  end
-
-  def profile
-    render json: @current_course
+  	for application in @applications
+  		return_array.add(application.user)
+  	end
   end
 
   private
@@ -49,22 +31,9 @@ class CoursesController < ApplicationController
     end
 
     # Only allow a trusted parameter "white list" through.
-    def user_params
+    def course_params
       params.require(:course).permit(
-        :utorid, 
-        :password, 
-        :password_confirmation,
-        :student_number,
-        :family_name,
-        :given_name,
-        :program,
-        :year,
-        :previous_ta_experience,
-        :phone_number,
-        :work_status,
-        :work_status_explain,
-        :department,
-        :department_explanation,
-        :date_of_application)
+        :course_code, 
+        :description)
     end
 end
--- spec/controllers/courses_controller_spec.rb
+++ spec/controllers/courses_controller_spec.rb
@@ -1,5 +1,17 @@
-require 'rails_helper'
+require_relative "../rails_helper"
+require_relative "../spec_helper"
 
 RSpec.describe CoursesController, type: :controller do
 
+	describe "GET index" do
+	    it "returns all users" do
+	      user = User.create!(utorid: "blah@gmail.com", password: "blah", password_confirmation: "blah")
+	      user2 = User.create!(utorid: "ah@gmail.com", password: "blah", password_confirmation: "blah")
+	      course = Course.create!(course_code: "CSC301", id: "1")
+	      app1 = TaApplication.create!(user_id: 2, course_id: "1")
+	      app2 = TaApplication.create!(user_id: 1, course_id: "1")
+	      get '/course/:id/applicants', id: 1
+	      expect(response.body).to include("blah@gmail.com")
+	    end
+	end
 end
